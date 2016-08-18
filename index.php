96bace5ec0cbd4e705c700b85d4223749da05002
Wily <wilyvasquez88@gmail.com>
2016-08-18 18:45:41 -0500

    cambios menores

--- index.php
+++ index.php
@@ -250,10 +250,9 @@
                 </div>
                 <div class="col-sm-6">
                     <ul class="pull-right">
-                        <li><a href="#">Home</a></li>
+                        <li><a href="index.php">Suzuki</a></li>
                         <li><a href="#">About Us</a></li>
-                        <li><a href="#">Faq</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="contacto.html">Contacto</a></li>
                     </ul>
                 </div>
             </div>
